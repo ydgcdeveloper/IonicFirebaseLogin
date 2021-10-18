7dee74e69adc3bd60f1e6a850ddb3f35a9b22028
Yan David <ygomezcoba@gmail.com>
2021-10-18 11:42:25 -0400

    Add and initialize firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {RouteReuseStrategy} from "@angular/router";
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import {IonicModule, IonicRouteStrategy} from "@ionic/angular";
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import {AppComponent} from "./app.component";
+import {AppRoutingModule} from "./app-routing.module";
+
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from "src/environments/environment";
+
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule, IonicModule.forRoot(), AppRoutingModule, 
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig))
+  ],
+  providers: [
+    {
+      provide: RouteReuseStrategy,
+      useClass: IonicRouteStrategy
+    }
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
