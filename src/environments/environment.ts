dd7cad25dd0ed0f95033189b5f10560d62e88bd9
Yan David <ygomezcoba@gmail.com>
2021-10-18 09:07:51 -0400

    Set firebase variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebaseConfig: {
+    apiKey: "AIzaSyBO1rQCZOMw6QH8L8xLBfkymugMQvywL_M",
+    authDomain: "login-9e697.firebaseapp.com",
+    projectId: "login-9e697",
+    storageBucket: "login-9e697.appspot.com",
+    messagingSenderId: "337403421266",
+    appId: "1:337403421266:web:22831df9e56e2e4c64f58b",
+    measurementId: "G-MQJ07RQ88P"
+  }
 };
 
 /*
@@ -13,4 +23,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+// import 'zone.js/dist/zone-error';   Included with Angular CLI.
