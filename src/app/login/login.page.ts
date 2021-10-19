284fc545c9fe4d871792382bbc42f2174b27306a
Yan David <ygomezcoba@gmail.com>
2021-10-18 22:53:22 -0400

    Add login form builder

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,16 +1,21 @@
 import {Component, OnInit} from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
 import {Router} from "@angular/router";
+import { LoginPageForm } from "./login.form.page";
 
 @Component({selector: "app-login", templateUrl: "./login.page.html", styleUrls: ["./login.page.scss"]})
 export class LoginPage implements OnInit {
-  private spinner: boolean;
 
-  constructor(private router : Router) {
+  private spinner: boolean;
+  form: FormGroup;
+
+  constructor(private router : Router, private formBuilder: FormBuilder) {
     this.spinner = false;
   }
 
   ngOnInit() {
     this.spinner = false;
+    this.form = new LoginPageForm(this.formBuilder).createForm();
   }
 
   login() {
