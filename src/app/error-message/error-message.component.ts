3e657926f5a75b562ee40bc49bf653acc3481f4d
Yan David <ygomezcoba@gmail.com>
2021-10-19 11:35:52 -0400

    Add field and error input to handle error message

--- src/app/error-message/error-message.component.ts
+++ src/app/error-message/error-message.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-error-message',
@@ -8,9 +9,18 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ErrorMessageComponent implements OnInit {
 
   @Input() message: string;
+  @Input() field: FormGroup;
+  @Input() error: string;
 
   constructor() { }
 
   ngOnInit() {}
+
+  shouldShowComponent(){
+    if(this.field.touched && this.field.errors?.[this.error]){
+      return true;
+    }
+    return false;
+  }
 
 }
