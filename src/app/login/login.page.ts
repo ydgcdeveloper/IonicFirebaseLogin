330b23fce626dd6e06615858841fd4e2bbcc6d66
Yan David <ygomezcoba@gmail.com>
2021-10-18 21:59:32 -0400

    Add spinner to login button

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
+import {Router} from "@angular/router";
 
-@Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
-})
+@Component({selector: "app-login", templateUrl: "./login.page.html", styleUrls: ["./login.page.scss"]})
 export class LoginPage implements OnInit {
+  private spinner: boolean;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private router : Router) {
+    this.spinner = false;
   }
 
+  ngOnInit() {
+    this.spinner = false;
+  }
+
+  login() {
+    this.spinner = true;
+    setTimeout(() => {
+      this.router.navigate(["home"]);
+    }, 1000);
+  }
 }
